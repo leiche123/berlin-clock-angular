8cca737a3fee53c5edbfa0f4b57922c490457bdb
leiche123 <tom.jasinski@gmx.de>
2022-03-10 23:39:31 +0100

    3 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,38 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { map, Observable, Subject, timer } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable, timer } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   public $time: Observable<Date> = timer(0, 1000).pipe(map(() => new Date()));
-  private unsubscribe$: Subject<void> = new Subject();
-  row1Elements = ['square', 'square', 'square', 'square'];
-  row2Elements = ['square', 'square', 'square', 'square'];
-  row3Elements = [
-    'square',
-    'square',
-    'square',
-    'square',
-    'square',
-    'square',
-    'square',
-    'square',
-    'square',
-    'square',
-    'square',
-    'square',
-  ];
-  row4Elements = ['square', 'square', 'square', 'square'];
+  fiveHourRowElements = new Array(4).fill(null);
+  singleHourRowElements = new Array(4).fill(null);
+  fiveMinuteRowElements = new Array(11).fill(null);
+  singleMinuteRowElements = new Array(4).fill(null);
   inkCircle = false;
-  inkRow1 = 0;
-  inkRow2 = 0;
-  inkRow3 = 0;
-  inkRow4 = 0;
+  inkFiveHourRow = 0;
+  inkSingleHourRow = 0;
+  inkFiveMinuteRow = 0;
+  inkSingleMinuteRow = 0;
 
   ngOnInit() {
-    this.$time.pipe(takeUntil(this.unsubscribe$)).subscribe((time) => {
+    this.$time.subscribe((time) => {
       const hours = time.getHours();
       const minutes = time.getMinutes();
       const seconds = time.getSeconds();
 
       this.inkCircle = !!(seconds % 2);
-      this.inkRow1 = Math.floor(hours / 5);
-      this.inkRow2 = hours % 5;
-      this.inkRow3 = Math.floor(minutes / 5);
-      this.inkRow4 = minutes % 5;
+      this.inkFiveHourRow = Math.floor(hours / 5);
+      this.inkSingleHourRow = hours % 5;
+      this.inkFiveMinuteRow = Math.floor(minutes / 5);
+      this.inkSingleMinuteRow = minutes % 5;
     });
   }
 
-  ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
+  isQuarterHourMark(index: number) {
+    return !((index + 1) % 3) && this.inkFiveMinuteRow > index;
   }
 }
